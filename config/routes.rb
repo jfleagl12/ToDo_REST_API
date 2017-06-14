26778a9faa01e4d7430ff930fcb22fb96f25419c
Jason Fleagle <jasonfleagle@hotmail.com>
2017-06-13 20:50:11 -0400

    Added in user spec and controllers, and application spec and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,14 @@ Rails.application.routes.draw do
     resources :items
   end
 end
+
+Rails.application.routes.draw do
+ 
+  post 'auth/login', to: 'authentication#authenticate'
+end
+
+
+Rails.application.routes.draw do
+  # [...]
+  post 'signup', to: 'users#create'
+end
